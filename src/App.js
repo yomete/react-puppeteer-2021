f033de660954e0c9c7b93f3d8d70f260100d3e72
Yomi Eluwande <iamlese@yahoo.com>
2021-06-20 22:59:47 +0200

    Add test for form submissions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+import "./App.css";
 
 function App() {
+  const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const login = (event) => {
+    event.preventDefault();
+
+    if (email === "username@gmail.com" && password === "password") {
+      setIsUserLoggedIn(true);
+    } else {
+      setIsUserLoggedIn(false);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="App-welcome-text">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="form-wrapper">
+        <h1 className="form-header">Login form</h1>
+
+        {!isUserLoggedIn && (
+          <form className="form" onSubmit={login}>
+            <input
+              type="email"
+              required
+              placeholder="Email Address"
+              className="form-input form-input__email"
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+            <input
+              type="password"
+              required
+              placeholder="Password"
+              className="form-input form-input__password"
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+            <button type="submit" className="form-submit-button">
+              Submit
+            </button>
+          </form>
+        )}
+
+        {isUserLoggedIn && <p className="form-success-message">You are now signed in.</p>}
+      </div>
     </div>
   );
 }
--- src/e2e.test.js
+++ src/e2e.test.js
@@ -9,12 +9,25 @@ describe("App.js", () => {
     page = await browser.newPage();
   });
 
-  it("contains the welcome text", async () => {
+  it("shows a success message after submitting a form", async () => {
     await page.goto("http://localhost:5000");
-    await page.waitForSelector(".App-welcome-text");
+    await page.waitForSelector(".form-header");
 
-    const text = await page.$eval(".App-welcome-text", (e) => e.textContent);
-    expect(text).toContain("Edit src/App.js and save to reload.");
+    await page.click(".form-input__email");
+    await page.type(".form-input__email", "username@gmail.com");
+
+    await page.click(".form-input__password");
+    await page.type(".form-input__password", "password");
+
+    await page.click(".form-submit-button");
+
+    await page.waitForSelector(".form-success-message");
+    const text = await page.$eval(
+      ".form-success-message",
+      (e) => e.textContent
+    );
+
+    expect(text).toContain("You are now signed in.");
   });
 
   afterAll(() => browser.close());
