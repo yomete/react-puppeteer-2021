25dff97b66f47c3e229efc81578c7c717f6d6f3a
Yomi Eluwande <iamlese@yahoo.com>
2021-06-20 18:32:24 +0200

    Add tests for route navigation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import logo from "./logo.svg";
+import "./App.css";
+
+function Home() {
+  return (
+    <p className="App-welcome-text">
+      Edit <code>src/App.js</code> and save to reload.
+    </p>
+  );
+}
+
+function About() {
+  return <p className="App-welcome-text">This is the about page.</p>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="App-welcome-text">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+
+          <div className="App-link-wrapper">
+            <Link className="App-link" id="home-page-link" to="/">
+              Home
+            </Link>
+            <Link className="App-link" id="about-page-link" to="/about">
+              About
+            </Link>
+          </div>
+        </header>
+      </div>
+    </Router>
   );
 }
 
--- src/e2e.test.js
+++ src/e2e.test.js
@@ -9,12 +9,15 @@ describe("App.js", () => {
     page = await browser.newPage();
   });
 
-  it("contains the welcome text", async () => {
+  it("navigates to the about page", async () => {
     await page.goto("http://localhost:5000");
     await page.waitForSelector(".App-welcome-text");
 
+    await page.click("#about-page-link");
+    await page.waitForSelector(".App-welcome-text");
+
     const text = await page.$eval(".App-welcome-text", (e) => e.textContent);
-    expect(text).toContain("Edit src/App.js and save to reload.");
+    expect(text).toContain("This is the about page.");
   });
 
   afterAll(() => browser.close());
