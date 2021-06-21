3d02ab72257235ad307b49b23f40c7061f8cb9a0
Yomi Eluwande <iamlese@yahoo.com>
2021-06-21 05:27:13 +0200

    add test for incorrect login details

--- src/e2e.test.js
+++ src/e2e.test.js
@@ -30,5 +30,23 @@ describe("App.js", () => {
     expect(text).toContain("You are now signed in.");
   });
 
+  it("shows an error message if authentication fails", async () => {
+    await page.goto("http://localhost:5000");
+    await page.waitForSelector(".form-header");
+
+    await page.click(".form-input__email");
+    await page.type(".form-input__email", "username@gmail.com");
+
+    await page.click(".form-input__password");
+    await page.type(".form-input__password", "password123");
+
+    await page.click(".form-submit-button");
+
+    await page.waitForSelector(".form-error-text");
+    const text = await page.$eval(".form-error-text", (e) => e.textContent);
+
+    expect(text).toContain("Please enter a correct username/password.");
+  });
+
   afterAll(() => browser.close());
 });
